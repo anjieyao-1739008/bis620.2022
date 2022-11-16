a9e2b984186578bc47e6125081e61ccae07fd11d
Carrie Yaoe <carrie.yao@yale.edu>
2022-11-16 16:32:30 -0500

    fix lint issues & increase test coverage

--- tests/testthat/test-plot.R
+++ tests/testthat/test-plot.R
@@ -2,7 +2,7 @@ test_that(
   "The accel_plot() returns a ggplot object.",
   {
     data(ukb_accel)
-    p =  accel_plot(ukb_accel[1:100,])
+    p <- accel_plot(ukb_accel[1:100, ])
     expect_true(inherits(p, "gg"))
   }
 )
@@ -19,7 +19,47 @@ test_that(
   "The accel_plot() is correct for time-series data.xy",
   {
     data(ukb_accel)
-    p = accel_plot(ukb_accel[1:100,])
-    expect_doppelganger("first-100-samples", p)
+    p <- accel_plot(ukb_accel[1:100, ])
+    vdiffr::expect_doppelganger("first-100-samples-time", p)
   }
 )
+
+
+test_that(
+  "The accel_plot() is correct for time-series data.xy",
+  {
+    x <- ukb_accel[1:100,]
+    ret <- map_dfc(x |> select(X, Y, Z),~ fft(.x, inverse = TRUE) |> Mod())
+    ret <- ret[seq_len(ceiling(nrow(ret)/2)), ]
+    longest_period <- as.numeric(difftime(max(x$time), min(x$time), units = "secs"))
+    shortest_period <- as.numeric(difftime(max(x$time[1:2]), min(x$time[1:2]), units = "secs"))
+    ret$freq <- 1 / seq(longest_period, shortest_period, length.out = nrow(ret))
+    ret
+    p <- accel_plot(ret)
+    vdiffr::expect_doppelganger("first-100-samples-freq", p)
+  }
+)
+
+
+test_that(
+  "The accel_plot() is correct for time-series data.xy",
+  {
+    data(ukb_accel)
+    s = spectral_signature(ukb_accel[1:100,], take_log = TRUE)
+    expect_true(inherits(s, "tbl_df"))
+  }
+)
+
+test_that(
+  "The accel_plot() is correct for time-series data.xy",
+  {
+    data(ukb_accel)
+    s = spectral_signature(ukb_accel[1:100,], take_log = FALSE)
+    expect_true(inherits(s, "tbl_df"))
+  }
+)
+
+
+
+
+#covr:: report()
